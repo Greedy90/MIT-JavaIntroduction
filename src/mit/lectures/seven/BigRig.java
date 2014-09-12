e0d78de7a6664fb52aa158000548e8e5a36709e4
Marco Terrinoni <marco.terrinoni90@gmail.com>
2014-09-12 07:12:59 +0200

    start seventh lesson

---
+++ src/mit/lectures/seven/BigRig.java
@@ -0,0 +1,9 @@
+package mit.lectures.seven;
+
+public class BigRig implements ICar {
+
+	public int getNumWheels() {
+		return 18;
+	}
+
+}
---
+++ src/mit/lectures/seven/ICar.java
@@ -0,0 +1,7 @@
+package mit.lectures.seven;
+
+public interface ICar {
+	boolean isCar = true;
+	
+	int getNumWheels();
+}
