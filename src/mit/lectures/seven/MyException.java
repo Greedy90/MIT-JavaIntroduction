b48c2ed537be7b0936be2ca09083a20583eb4322
Marco Terrinoni <marco.terrinoni90@gmail.com>
2014-09-13 07:56:23 +0200

    suppress warning

--- src/mit/lectures/seven/MyException.java
+++ src/mit/lectures/seven/MyException.java
@@ -1,5 +1,6 @@
 package mit.lectures.seven;
 
+@SuppressWarnings("serial")
 public class MyException extends Exception {
 	/*public Object get(int index) throws ArrayOutOfBoundsException {
 		if (index < 0 || index > size())
